59cdfaaabee603685073eb0c94c2bc43d8dea792
Joshua Bowers <joshua.bowers+code@gmail.com>
2019-06-07 17:05:49 -0700

    Adding in the react frontend; largely vanilla create-react-app

--- index.js
+++ index.js
@@ -22,6 +22,16 @@ app.get('/api/articles', async (req, res, next) => {
   }
 })
 
+const path = require('path')
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')))
+
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Express server listening on port ${ PORT }`)
