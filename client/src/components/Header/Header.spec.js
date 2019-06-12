91192f166a4172d13f683a452e9cb52d8c9daee2
Joshua Bowers <joshua.bowers+code@gmail.com>
2019-06-12 14:22:39 -0700

    Have added in enzyme to facilitate testing.

---
+++ client/src/components/Header/Header.spec.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Header from './Header.component';
+
+it('renders without crashing', () => {
+  shallow(<Header title="Articles" />);
+});
+
+it('renders the title', () => {
+  const title = 'Articles';
+  const wrapper = shallow(<Header title={title} />);
+  const header = <h1>{title}</h1>;
+
+  expect(wrapper).toContainReact(header);
+});
