d974118fc30eea7b63d81ebb385bca143757e5da
Joshua Bowers <joshua.bowers+code@gmail.com>
2019-07-17 17:57:26 -0700

    Attempting to set up the server to use ES6 modules, as I find them less confusing.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const express = require('express')
-const cors = require('cors')
+import express from 'express';
+import cors from 'cors';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express()
+const __dirname = dirname( fileURLToPath( import.meta.url ) )
 
 // TODO: create a directory, server, for API routes
 
@@ -31,14 +34,13 @@ app.post('/api/articles', async (req, res, next) => {
   }
 })
 
-const path = require('path')
-
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'client/build')))
+app.use(express.static(join(__dirname, '/client/build')))
 
 // Anything that doesn't match the above, send back index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+  console.log(__dirname)
+  res.sendFile(join(__dirname, '/client/build/index.html'))
 })
 
 const PORT = process.env.PORT || 5000
