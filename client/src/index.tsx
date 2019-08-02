c84ecce801ff463e5e280d25ec8853d307a21cd1
Joshua Bowers <joshua.bowers+code@gmail.com>
2019-08-02 15:41:36 -0700

    Redux store is now persisted
    Via redux-persist and localforage, the redux store is now persisted
    across page reloads, which helps with, amongst other things, session
    management.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,15 +7,18 @@ import { configureStore } from './store';
 import { Provider } from 'react-redux';
 import { history } from 'store/history';
 import { Router } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
-const store = configureStore();
+const { store, persistor } = configureStore();
 
 const renderApp = () =>
   ReactDOM.render(
     <Provider store={store}>
-      <Router history={history}>
-        <App />
-      </Router>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router history={history}>
+          <App />
+        </Router>
+      </PersistGate>
     </Provider>,
     document.getElementById('root')
   );
--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,15 +1,27 @@
 import { createStore, applyMiddleware, Middleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
+import { persistStore, persistReducer } from 'redux-persist';
+import autoMergeLevel2 from 'redux-persist/es/stateReconciler/autoMergeLevel2';
+import * as storage from 'localforage';
 import { rootReducer, AppState } from './reducers';
 import { historyChangedMiddleware } from './history';
 import { rootSaga } from './sagas';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  stateReconciler: autoMergeLevel2
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const configureStore = (preloadedState?: AppState) => {
+  const environment = process.env.NODE_ENV;
   const sagaMiddleware = createSagaMiddleware();
   const middlewares: Middleware[] = [historyChangedMiddleware, sagaMiddleware];
 
-  if (process.env.NODE_ENV === 'development') {
+  if (environment === 'development') {
     const reduxLogger = require('redux-logger');
 
     middlewares.push(reduxLogger.logger as Middleware);
@@ -20,13 +32,21 @@ export const configureStore = (preloadedState?: AppState) => {
   const enhancers = [middlewareEnhancer];
   const composedEnhancers = composeWithDevTools(...enhancers);
 
-  const store = createStore(rootReducer, preloadedState, composedEnhancers);
+  const store = createStore(
+    persistedReducer,
+    preloadedState,
+    composedEnhancers
+  );
+  const persistor = persistStore(store);
 
-  if (process.env.NODE_ENV !== 'production' && module.hot) {
-    module.hot.accept('./reducers', () => store.replaceReducer(rootReducer));
+  if (environment !== 'production' && module.hot) {
+    module.hot.accept('./reducers', () => {
+      const nextRootReducer = require('./reducers').rootReducer;
+      store.replaceReducer(persistReducer(persistConfig, nextRootReducer));
+    });
   }
 
   sagaMiddleware.run(rootSaga);
 
-  return store;
+  return { store, persistor };
 };
